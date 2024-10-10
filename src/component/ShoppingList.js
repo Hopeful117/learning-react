1a56b07529c0d354ec2c56f601799202a8afe52b
Hopeful117 <135059935+Hopeful117@users.noreply.github.com>
2024-10-10 18:59:28 +0200

    update

--- src/component/ShoppingList.js
+++ src/component/ShoppingList.js
@@ -9,8 +9,9 @@ const plantList = [
 function ShoppingList() {
     return (
         <ul>
-            {plantList.map((plant) => (
-                <li>{plant}</li>
+            {plantList.map((plant, index) => (
+                
+                <li key={`${plant}-${index}`}>{ plant }</li>
             ))}
         </ul>
     )
